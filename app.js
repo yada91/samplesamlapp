7e8d022c848a68672d083eb50dcd6eec9c8b6656
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2019-01-25 13:04:53 -0500

    added get

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.post('/login',
+app.get('/login',
     passport.authenticate('saml', {
         successRedirect: '/',
         failureRedirect: '/login',

f64edf03518fa47fd051c3dedf0b8907b7918f74
이동후 <nap@nate.com>
2021-04-23 15:06:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -20,26 +20,11 @@ passport.deserializeUser(function (user, done) {
 passport.use(new SamlStrategy(
     {
       // path: '/login/callback',
-        entryPoint: 'https://login.microsoftonline.com/afbe2a25-78bd-4193-8279-95142c1f0169/saml2',
-        issuer: 'https://samplesamlapp.herokuapp.com',
-        callbackUrl: 'https://samplesamlapp.herokuapp.com/login/callback',
+        entryPoint: 'http://pmk.wiseservice.io:9999/saml',
+        issuer: 'https://pmi-e--qa.my.salesforce.com',
+        callbackUrl: 'https://qa-pmk.cs117.force.com/ConsumerSupport/login',
         // cert: fs.readFileSync('./SampleSAMLApp.cer', 'utf-8'),
-        cert: 'MIIC8DCCAdigAwIBAgIQOPFi8TlvhIxE0uGtBqOw/jANBgkqhkiG9w0BAQsFADA0\n' +
-            'MTIwMAYDVQQDEylNaWNyb3NvZnQgQXp1cmUgRmVkZXJhdGVkIFNTTyBDZXJ0aWZp\n' +
-            'Y2F0ZTAeFw0xOTAxMjQyMTA2NDdaFw0yMjAxMjQyMTA2NDdaMDQxMjAwBgNVBAMT\n' +
-            'KU1pY3Jvc29mdCBBenVyZSBGZWRlcmF0ZWQgU1NPIENlcnRpZmljYXRlMIIBIjAN\n' +
-            'BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2/7Gk6BN7rvWV2EyrU9rX5/8xSmo\n' +
-            'MYgYVqTzh+gFiVzJU1TC6CYybMg+xpouTLDheoUfbDw/0ZsL6qpxYTwd7fUOZ4AC\n' +
-            'TzjAOC/W0WDWzhllnMrxE/OgWn6+ihCKkj898wn7AIa54PEYUHGwJHZzFMuO1IFW\n' +
-            'TwDQSUNHRe5RuwC2TMPAtVhkGXXCNH6MOAiH+2WlGcPRTZwlxomS5vfqeeoWpw0Y\n' +
-            'mtlEW8KGTcTmp73Dj4ijn/cN9/iVdYk9M3Gv9810jknYRG+9c6FNcC/sd1gKFbIM\n' +
-            'qHcGBJumv1eTjrmetnE6/TSUgfWlQcFw4q4Uu9gisFgvneC5pOP4SGrgewIDAQAB\n' +
-            'MA0GCSqGSIb3DQEBCwUAA4IBAQCPk9WaC4wlyDJOQaL7qljsFOdW8inZQsRN0sFV\n' +
-            '+7bkVgeshNbxyvv3bDw3NkJ/eatf2VkAN7jL03HOzi4jftNli0kz2ltCnZUTSo98\n' +
-            'TUK6PYTOzp0z2vYLchS77xNh0zxfc02fT/PePp2mYc1rv8h4rmnTG5tNOx81t2UE\n' +
-            '1rwekljWAY2bNVb3CnwHwZeAb25xLMdUyBF/Mv+UV1mIbcNeCG2XaKzhL8mPB4fy\n' +
-            'nqLMT+UEohPbVN5/WEw8cWtXKfiK1IqDZo11+EHdGafIhcAm8ySUpdtgsA8cunRx\n' +
-            'Pf2wNRigBFKLJwP2UkoVwAgS1DmMvhY6nTk2KJN/Q+bLbkFr',
+         cert: fs.readFileSync('./Certificates_for_GC.crt', 'utf-8'),
         // authnContext: 'http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/windows',
         identifierFormat: null
         // signatureAlgorithm: 'sha256'
